aba951b88aab4408a4b8a6ce5aad17493696d61d
Gabriel Raiol <raiol.gabriel@hotmail.com>
2023-10-09 17:16:16 -0300

    adding conversion feature; adding useEffect to 'selected' state on the stopwatch

--- src/components/Stopwatch/index.tsx
+++ src/components/Stopwatch/index.tsx
@@ -1,13 +1,30 @@
 import Button from "../Button";
 import Watch from "./Watch";
 import style from "./Stopwatch.module.scss"
+import { timeToSeconds } from "../../common/utils/time";
+import { ITask } from "../../types/ITask";
+import { useEffect, useState} from 'react';
 
-function Stopwatch() {
+interface Props {
+    selected: ITask | undefined
+}
+
+function Stopwatch({selected}: Props) {
+    
+    const [time, setTime] = useState <number>();
+
+    useEffect(() => {
+        if (selected?.time) {
+            setTime(timeToSeconds(selected.time))
+        }    
+    }, [selected])
+    
     return(
         <div className={style.stopwatch}>
             <p className={style.title}>
                 Escolha um card e inicie o cronômetro
             </p>
+                Tempo: {time}
             <div className={style.watchWrapper}>
                 <Watch />
             </div>
